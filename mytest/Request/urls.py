67ef1ca76dd5d84db47f383b0ad2b9500ca5217a
Lextor <lextor@ukr.net>
2011-01-20 22:43:11 +0200

    create link from in page to request list page, some changes in code according to PEP8

--- mytest/Request/urls.py
+++ mytest/Request/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('mytest.Request.views',
-    url(r'^$', 'request_list', name='request_list'),
+    url(r'^$', 'request_list', name='request-list'),
 )
