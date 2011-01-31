d587de858a5ae33492991760ea4651fc7ca2f5e2
Lextor <lextor@ukr.net>
2011-01-31 13:28:15 +0200

    kill max_lenght parametr in TextField

--- mytest/persons/models.py
+++ mytest/persons/models.py
@@ -5,11 +5,11 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     date_of_birth = models.DateField(blank=True, null=True)
-    about = models.TextField(max_length=500, blank=True)
+    about = models.TextField(blank=True)
     mail_address = models.EmailField(blank=True)
     jabber = models.EmailField(blank=True)
     skype = models.CharField(max_length=30)
-    other_contacts = models.TextField(max_length=500, blank=True)
+    other_contacts = models.TextField(blank=True)
 
     def __unicode__(self):
         return u'%s %s' % (self.last_name, self.first_name)
