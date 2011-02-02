8cb49489c54fe68c7248efd9ff3b9fafe0fe9979
Lextor <lextor@ukr.net>
2011-02-02 14:07:37 +0200

    add URLs for login, add login.html template

--- mytest/urls.py
+++ mytest/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     (r'^$', 'mytest.views.index'),
     (r'^reqlist/$', include('mytest.requests.urls')),
     (r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='auth_login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
 )
