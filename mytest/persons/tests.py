4010eaa328b7e9ca9106b8bcba5fe9e2f12b5399
Lextor <lextor@ukr.net>
2011-02-04 12:59:54 +0200

    add tests, some changes in template

--- mytest/persons/tests.py
+++ mytest/persons/tests.py
@@ -7,6 +7,7 @@ Replace these with more appropriate tests for your application.
 from django.test import TestCase
 from django.test.client import Client
 from mytest.persons.models import Person
+from django.contrib.auth.models import User
 
 
 class SimpleTest(TestCase):
@@ -25,12 +26,6 @@ True
 
 
 class PersonTest(TestCase):
-    def setUp(self):
-        p = Person()
-        p.first_name = 'Alexander'
-        p.last_name = 'Tsishkovsky'
-        p.skype = 'lextor_alex'
-        p.save()
 
     def testBasic(self):
         c = Client()
@@ -39,9 +34,29 @@ class PersonTest(TestCase):
 
 
 class ContextProcTest(TestCase):
-
     def testBasic(self):
         c = Client()
         response = c.get('/')
         self.assertEqual(response.context['settings'].TIME_ZONE, 'Ukraine/Kiev')
         self.assertNotEqual(response.content.find('django.db.backends.sqlite3'), -1)
+
+class EditFormTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('lex', 'lex@mail.ru', '2243')
+        user.save()
+      
+
+    def testBasic(self):
+        c = Client()
+        response = c.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/edit/', follow=True)
+        self.assertEqual(response.redirect_chain, [('http://testserver/accounts/login/?next=/edit/', 302)])
+        response = c.login(username='lex', password='2243')
+        self.assertTrue(response)
+        response = c.get('/edit/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.content.find('Alexander'), -1)
+        response = c.post('/edit/', {'first_name': 'Newname', 'last_name': 'Newlastn', 'skype': '1'})
+        response = c.get('/')
+        self.assertNotEqual(response.content.find('Newname'), -1)
