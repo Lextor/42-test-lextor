f14468105543320d766129083b62f83b09ab2f9f
Lextor <lextor@ukr.net>
2011-02-12 19:25:19 +0200

    fix in style

--- mytest/utils/management/commands/project_info.py
+++ mytest/utils/management/commands/project_info.py
@@ -1,5 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError, NoArgsCommand
-from django.conf import settings
+from django.core.management.base import NoArgsCommand
 from django.db.models import get_models
 
 
@@ -11,4 +10,3 @@ class Command(NoArgsCommand):
             self.stdout.write(n)
             self.stderr.write("Error: ")
             self.stderr.write(n)
-
